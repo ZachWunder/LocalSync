365c7c19f1edc1dad4078b69e81bfb30af90daf7
Zach Wunder <zachwunder@gmail.com>
2020-12-29 10:53:16 -0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Syncs from S3 to local directory (Pull)
-AWS_PROFILE=personalsync aws s3 sync s3://thebrain /path/to/local
+# $1 = Local Directory 
+# $2 = S3 Bucket (don't include s3://)
 
-# Syncs local directory to S3 (Push)
-AWS_PROFILE=personalsync aws s3 sync /path/to/local s3://thebrain --exclude '.DS_STORE' --exclude '.git' --exclude '.obsidian'
+# Watch local dir
+inotifywait -e close_write,moved_to,create -m $1 |
+while read -r directory ; do
+  # Syncs from S3 to local directory (Pull)
+  AWS_PROFILE=personalsync aws s3 sync s3://$2 $1
+
+  # Syncs local directory to S3 (Push)
+  AWS_PROFILE=personalsync aws s3 sync $1 s3://$2 --exclude '.DS_STORE' --exclude '.git' --exclude '.obsidian'
+done
+
